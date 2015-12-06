ef327921e0c6b1c338a6f725440d3f58ddda0d3a
AlphaRhoG <ahdhar@gmail.com>
2015-12-05 21:34:13 -0600

    adding run script

---
+++ apps/gaussian/run_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Fan2"
+COUNTER=1
+while [  $COUNTER -lt 100 ]; do
+        nvprof --normalized-time-unit us -s ./gaussian -q -s $COUNTER 2>&1 >/dev/null | grep Fan2 | awk -F" " '{print $2" "$3" " $4}'
+        let COUNTER=COUNTER*10
+done
+
+COUNTER=128
+while [  $COUNTER -lt 8192 ]; do
+        nvprof --normalized-time-unit us -s ./gaussian -q -s  $COUNTER 2>&1 >/dev/null | grep Fan2 | awk -F" " '{print $2" "$3" "$4}'
+        let COUNTER=COUNTER*2
+done
+
+echo "Fan1"
+COUNTER=1
+while [  $COUNTER -lt 100 ]; do
+        nvprof --normalized-time-unit us -s ./gaussian -q -s $COUNTER 2>&1 >/dev/null | grep Fan1 | awk -F" " '{print $2" "$3" " $4}'
+        let COUNTER=COUNTER*10
+done
+
+COUNTER=128
+while [  $COUNTER -lt 8192 ]; do
+        nvprof --normalized-time-unit us -s ./gaussian -q -s  $COUNTER 2>&1 >/dev/null | grep Fan1 | awk -F" " '{print $2" "$3" "$4}'
+        let COUNTER=COUNTER*2
+done
+
+
+COUNTER=1
+while [  $COUNTER -lt 100 ]; do
+        echo $COUNTER
+       let COUNTER=COUNTER*10
+done
+COUTER=128
+while [  $COUNTER -lt 8192 ]; do
+        echo $COUNTER
+       let COUNTER=COUNTER*2
+done
