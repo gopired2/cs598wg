40c0134fcdf853a4d15522e5c5404b027791e82a
AlphaRhoG <ahdhar@gmail.com>
2015-12-05 22:20:57 -0600

    updated run script

--- apps/gaussian/run_script.sh
+++ apps/gaussian/run_script.sh
@@ -3,12 +3,14 @@
 echo "Fan2"
 COUNTER=1
 while [  $COUNTER -lt 100 ]; do
+	echo $COUNTER
         nvprof --normalized-time-unit us -s ./gaussian -q -s $COUNTER 2>&1 >/dev/null | grep Fan2 | awk -F" " '{print $2" "$3" " $4}'
         let COUNTER=COUNTER*10
 done
 
 COUNTER=128
 while [  $COUNTER -lt 8192 ]; do
+	echo $COUNTER
         nvprof --normalized-time-unit us -s ./gaussian -q -s  $COUNTER 2>&1 >/dev/null | grep Fan2 | awk -F" " '{print $2" "$3" "$4}'
         let COUNTER=COUNTER*2
 done
@@ -16,24 +18,15 @@ done
 echo "Fan1"
 COUNTER=1
 while [  $COUNTER -lt 100 ]; do
+	echo $COUNTER
         nvprof --normalized-time-unit us -s ./gaussian -q -s $COUNTER 2>&1 >/dev/null | grep Fan1 | awk -F" " '{print $2" "$3" " $4}'
         let COUNTER=COUNTER*10
 done
 
 COUNTER=128
 while [  $COUNTER -lt 8192 ]; do
+	echo $COUNTER
         nvprof --normalized-time-unit us -s ./gaussian -q -s  $COUNTER 2>&1 >/dev/null | grep Fan1 | awk -F" " '{print $2" "$3" "$4}'
         let COUNTER=COUNTER*2
 done
 
-
-COUNTER=1
-while [  $COUNTER -lt 100 ]; do
-        echo $COUNTER
-       let COUNTER=COUNTER*10
-done
-COUTER=128
-while [  $COUNTER -lt 8192 ]; do
-        echo $COUNTER
-       let COUNTER=COUNTER*2
-done
